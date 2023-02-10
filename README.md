43934e40f2a43db2cbd9c70d71717dae205713e4
Lucas Ramos <lucasramoslobo83@gmail.com>
2023-02-10 15:41:27 -0300

    adiciona sistema de login na api

---
+++ README.md
@@ -0,0 +1,23 @@
+# Django-React-Api-GameStore
+
+Api feita em Django que é acessada pelo Fronted construido em React Web
+
+## 🚀 Backend
+
+Para iniciar basta instalar as dependências em requirements.txt e iniciar a api com os comandos:
+
+*cd ecommerce_api
+
+*pip install -r requirements.txt
+
+*python manage.py runserver
+
+### 📋 Frontend
+
+Para iniciar o Frontend é nescessário ter iniciado o Backend e escrever os comandos:
+
+*cd frontend/my_app
+
+*npm install
+
+*npm start
--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
 import ProductList from './components/Listcard';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<ProductList />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
+      <ProductList />
     </div>
   );
 }
